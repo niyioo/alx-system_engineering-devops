4ef705762c1d07815920a8aa3f7d1dfe42857c86
niyioo <niyiolagbegi@gmail.com>
2023-08-01 13:36:07 +0100

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+
+# Check if the argument matches the regular expression
+def match_school(input)
+  regex = /School/
+  if input.match?(regex)
+    puts "The input contains 'School'."
+  else
+    puts "The input does not contain 'School'."
+  end
+end
+
+# Get the argument from the command line
+if ARGV.empty?
+  puts "Please provide an argument."
+else
+  input_argument = ARGV[0]
+  match_school(input_argument)
+end
