496f8b26229f21ccc87ea396ec6c945cda87f20d
niyioo <niyiolagbegi@gmail.com>
2023-09-22 04:48:08 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,15 +1,7 @@
-# Puppet manifest to kill a process named "killmenow"
+# Puppet manifest to kill a process named killmenow
 
-exec { 'kill_killmenow':
-  command     => 'pkill killmenow',
-  onlyif      => 'pgrep killmenow',
+exec { 'killmenow':
+  command     => 'pkill -f "killmenow"',
+  path        => '/usr/bin:/bin:/usr/sbin:/sbin',
   refreshonly => true,
-  subscribe   => Exec['start_my_process'],
-  path        => '/usr/bin:/bin',
-}
-
-exec { 'start_my_process':
-  command => 'your_start_command_here',
-  onlyif  => 'pgrep killmenow',
-  path    => '/usr/bin:/bin',
 }
