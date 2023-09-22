00f93724ba5b4e10d49e723ad38061ec3b93e4b7
niyioo <niyiolagbegi@gmail.com>
2023-09-22 04:25:39 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet manifest to create a file at /tmp/school
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet\n',
+}
