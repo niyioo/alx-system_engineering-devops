47bd88e2c1c505406071b0ef3720b0fc0c423b1c
niyioo <niyiolagbegi@gmail.com>
2023-09-25 04:55:11 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Puppet manifest to configure SSH client
 
-file { 'ect/ssh/ssh-config':
+file { 'ect/ssh/ssh-cofig':
 	ensure => present,
 
 content =>"
@@ -10,6 +10,6 @@ content =>"
 	host*
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
-	",
+	".
 
 }
