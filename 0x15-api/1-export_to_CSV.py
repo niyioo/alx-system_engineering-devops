0b30ed27d072f6daa239037e4578ecbd72ba529a
niyioo <niyiolagbegi@gmail.com>
2023-10-23 08:34:01 -0700

    l

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,66 +1,39 @@
 #!/usr/bin/python3
-"""
-This script retrieves and exports an employee's TODO list
-progress from a REST API to a CSV file.
-"""
+""" Retrieve and display an employee's TODO list progress from a
+REST API and export in CSV format"""
 import csv
 import requests
 import sys
 
 
-def get_employee_todo_progress_to_csv(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    user_url = f'{base_url}users/{employee_id}'
-    todo_url = f'{base_url}todos?userId={employee_id}'
-
-    try:
-        # Retrieve user information
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
-        user_id = user_data.get('id')
-        user_name = user_data.get('name')
-
-        # Retrieve TODO list for the employee
-        todo_response = requests.get(todo_url)
-        todo_data = todo_response.json()
-
-        # Prepare CSV file name
-        csv_file_name = f"{user_id}.csv"
-
-        # Write data to CSV file
-        with open(csv_file_name, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-            csv_writer.writerow([
-                "USER_ID",
-                "USERNAME",
-                "TASK_COMPLETED_STATUS",
-                "TASK_TITLE"
-            ])
-
-            for task in todo_data:
-                csv_writer.writerow([
-                    user_id,
-                    user_name,
-                    task['completed'],
-                    task['title']
-                ])
-
-        print(f"Data has been exported to {csv_file_name}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"API request error: {e}")
-        sys.exit(1)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
+    base_url = 'https://jsonplaceholder.typicode.com/'
 
     employee_id = sys.argv[1]
 
-    if not employee_id.isdigit():
-        print("Employee ID must be an integer.")
-        sys.exit(1)
+    user_url = '{}users/{}'.format(base_url, employee_id)
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    username = user_data.get('username')
 
-    get_employee_todo_progress_to_csv(int(employee_id))
+    print("Employee {} is done with tasks".format(username), end="")
+
+    todo_url = '{}todos?userId={}'.format(base_url, employee_id)
+    todo_response = requests.get(todo_url)
+    tasks = todo_response.json()
+
+    task_list = []
+    for task in tasks:
+        task_list.append([employee_id,
+                          username,
+                          task.get('completed'),
+                          task.get('title')])
+
+    filename = '{}.csv'.format(employee_id)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        for task in task_list:
+            employee_writer.writerow(task)
