ab24b2c142406e8b29fa8a0806d87263ac6baabf
niyioo <niyiolagbegi@gmail.com>
2023-11-07 08:45:21 +0100

    1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of
+the first 10 hot posts for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Print the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    # Set a custom User-Agent to avoid Too Many Requests errors
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+
+    # Define the API endpoint URL for hot posts in the subreddit
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+
+    # Send an HTTP GET request to the Reddit API
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response and 
+        # extract the titles of the first 10 hot posts
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        # Invalid subreddit or other error, print None
+        print(None)
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        top_ten(subreddit)
